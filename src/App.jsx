09f52095883db6d59fce84913d1699c400a2c18e
Tautminas Cibulskis <tautminas@yahoo.com>
2023-11-24 00:07:17 +0200

    Add 21 buttons in total

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import React, { useState } from "react";
+import "./styles.css";
+
 function App() {
-  return <h1>Habit marker</h1>;
+  const totalButtons = 21;
+  const initialColor = "#FFFFCC";
+
+  const initialButtonColors = Array.from(
+    { length: totalButtons },
+    () => initialColor
+  );
+
+  const [buttonColors, setButtonColors] = useState(initialButtonColors);
+
+  const changeColor = (index) => {
+    const newColors = [...buttonColors];
+    newColors[index] =
+      newColors[index] === initialColor ? "#FFD700" : initialColor;
+    setButtonColors(newColors);
+  };
+
+  return (
+    <div>
+      <h1>Toggle Color Buttons</h1>
+      {buttonColors.map((color, index) => (
+        <button
+          key={index}
+          className="round-button"
+          style={{ backgroundColor: color }}
+          onClick={() => changeColor(index)}
+        >
+          {index + 1}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default App;
