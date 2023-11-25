3e987fe0df0533fd6ff8e57a7ef199b1ca69744e
Tautminas Cibulskis <tautminas@yahoo.com>
2023-11-25 21:55:33 +0200

    Add possibility to expand and collapse

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 function App() {
-  const totalButtons = 21;
+  const totalButtons = 100; // Total buttons available
   const initialButtonState = 0;
 
   const initialButtonStates = Array.from(
@@ -14,11 +14,16 @@ function App() {
     JSON.parse(localStorage.getItem("buttonStates")) || initialButtonStates;
 
   const [buttonStates, setButtonStates] = useState(storedButtonStates);
+  const [displayedButtons, setDisplayedButtons] = useState(21); // Initially displaying 21 buttons
 
   const toggleButton = (index) => {
     const newStates = [...buttonStates];
     newStates[index] = newStates[index] === 0 ? 1 : 0;
     setButtonStates(newStates);
+  };
+
+  const handleExpandCollapse = () => {
+    setDisplayedButtons(displayedButtons === 21 ? 100 : 21);
   };
 
   useEffect(() => {
@@ -28,15 +33,20 @@ function App() {
   return (
     <div>
       <h1>Toggle Buttons</h1>
-      {buttonStates.map((state, index) => (
-        <button
-          key={index}
-          className={`round-button ${state === 1 ? "toggled" : ""}`}
-          onClick={() => toggleButton(index)}
-        >
-          {index + 1}
-        </button>
-      ))}
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {buttonStates.slice(0, displayedButtons).map((state, index) => (
+          <button
+            key={index}
+            className={`round-button ${state === 1 ? "toggled" : ""}`}
+            onClick={() => toggleButton(index)}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+      <button onClick={handleExpandCollapse}>
+        {displayedButtons === 21 ? "Expand" : "Collapse"}
+      </button>
     </div>
   );
 }
